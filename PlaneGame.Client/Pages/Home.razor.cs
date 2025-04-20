908a22f4b89c494e8ea9e1bc6939f39d4bef5ddd
Islam Sissaoui <islsis@ppdgroup.com>
2025-04-19 21:19:18 -0400

    Add project files.

---
+++ PlaneGame.Client/Pages/Home.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PlaneGame.Client.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        /// <summary>
+        /// Recursive Backtracking
+        /// </summary>
+        void GenerateMaze() 
+        {
+        }
+    }
+}
