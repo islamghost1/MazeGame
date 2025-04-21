6f63b92e6e1d909b6d0a55b66088e4d57146150b
Islam Sissaoui <islsis@ppdgroup.com>
2025-04-21 02:23:47 -0400

    typo

--- PlaneGame.Client/Models/InputModels.cs
+++ PlaneGame.Client/Models/InputModels.cs
@@ -5,10 +5,10 @@ namespace PlaneGame.Client.Models
     public class DimentionsModel
     {
         [Required]
-        [Range(5, 40, ErrorMessage = "Width must be between 5 and 40.")]
+        [Range(5, 100, ErrorMessage = "Width must be between 5 and 100.")]
         public int Width { get; set; }
         [Required]
-        [Range(5, 40, ErrorMessage = "Width must be between 5 and 40.")]
+        [Range(5, 100, ErrorMessage = "Height must be between 5 and 100.")]
         public int Height { get; set; }
     }
 }
